13f671eaca55a89d6228e1c51d3d7d1495661b66
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-20 10:55:14 +0300

    Substact method impelemeted

--- server/main.go
+++ server/main.go
@@ -17,3 +17,8 @@ func (s *calculatorServer) Add(ctx context.Context, req *calculator.AddRequest) 
 	result := req.Num1 + req.Num2
 	return &calculator.Response{Response: result}, nil
 }
+
+func (s *calculatorServer) Subtract(ctx context.Context, req *calculator.SubtractRequest) (*calculator.Response, error) {
+	result := req.Num1 - req.Num2
+	return &calculator.Response{Response: result}, nil
+}
